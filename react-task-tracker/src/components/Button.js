133ade82f016c4aed8d5a8d78946d96bbcdd0bfe
Elliot <elliotshuck@protonmail.ch>
2021-09-14 23:13:12 +0100

    Working button
    added a simple working button

--- react-task-tracker/src/components/Button.js
+++ react-task-tracker/src/components/Button.js
@@ -1,11 +1,26 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const Button = () => {
-    return (
-        <div>
-            <button className='btn'>Add</button>
-        </div>
-    )
+const Button = ({ color, text }) => {
+    const onClick = () => {
+        console.log('click')
+    }
+
+
+
+    return <button onClick={onClick} style={{backgroundColor: color}} className='btn'>{text}</button>
+   
+}
+
+
+Button.defaultProps = {
+    color: 'steelblue',
+}
+
+Button.propTypes = {
+    text: PropTypes.string,
+    color: PropTypes.string,
+
 }
 
 export default Button
