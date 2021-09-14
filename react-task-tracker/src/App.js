d5229b06d6dba70741e926ed6fd75124766789d7
Elliot <elliotshuck@protonmail.ch>
2021-09-14 22:58:26 +0100

    Adding CSS

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,15 +1,22 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Header from './components/Header'
+
 
 function App() {
-  const name = 'brad'
-  const x = false
+  // const name = 'brad'
+  // const x = false
   return (
-    <div className="container">
-      <h1>Hello From React </h1>
-      
+    <div className='container'>
+      <Header />  
     </div>
   );
 }
+
+// Header.defaultProps = {
+//   title: 'Task Tracker',
+// }
+
 
 export default App;
